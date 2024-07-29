9845ce2be3b4144e7621107e6e2d77ce5fc80e6d
BaeJooHwan <kyoung3774@naver.com>
2024-07-29 17:47:21 +0900

    2024.07.29

--- 06_portfolio-site/js/main.js
+++ 06_portfolio-site/js/main.js
@@ -5,7 +5,7 @@ console.log(spyEls);
 spyEls.forEach(function (spyEl) {
   new ScrollMagic.Scene({ // 감시할 장면 추가 및 옵션 지정
     triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
-    triggerHook: 0.5 // 화면의 50% 지점에서 보여짐 여부 감시(0~1 지정)
+    triggerHook: 0.3 // 화면의 50% 지점에서 보여짐 여부 감시(0~1 지정)
 
   })
   .setClassToggle(spyEl,'show') // 요소가 화면에 보이면 show 클래스 추가
