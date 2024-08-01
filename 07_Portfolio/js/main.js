03212a0246b7cd70fb55a7728822d66af1874996
BaeJooHwan <kyoung3774@naver.com>
2024-08-01 17:54:21 +0900

    안해!

--- 07_Portfolio/js/main.js
+++ 07_Portfolio/js/main.js
@@ -1,47 +1,53 @@
 
-let page1 = document.querySelector('.menu_btn ul li:nth-child(1)')
-let page2 = document.querySelector('.menu_btn ul li:nth-child(2)')
-let page3 = document.querySelector('.menu_btn ul li:nth-child(3)')
-let page4 = document.querySelector('.menu_btn ul li:nth-child(4)')
-let page5 = document.querySelector('.menu_btn ul li:nth-child(5)')
+let btn1 = document.querySelector('.menu_btn ul li:nth-child(1)')
+let btn2 = document.querySelector('.menu_btn ul li:nth-child(2)')
+let btn3 = document.querySelector('.menu_btn ul li:nth-child(3)')
+let btn4 = document.querySelector('.menu_btn ul li:nth-child(4)')
+let btn5 = document.querySelector('.menu_btn ul li:nth-child(5)')
 
-page1.addEventListener('click', function(){
-  style.display ="adsolute"
+let page1 = document.querySelector('.main_page-1')
+let page2 = document.querySelector('.main_page-2')
+let page3 = document.querySelector('.main_page-3')
+let page4 = document.querySelector('.main_page-4')
+let page5 = document.querySelector('.main_page-5')
+
+btn1.addEventListener('click', function(){
+  page1.style.display="block"
   page2.style.display="none"
   page3.style.display="none"
   page4.style.display="none"
   page5.style.display="none"
 })
 
-page2.addEventListener('click', function(){
-  style.display ="adsolute"
+btn2.addEventListener('click', function(){
   page1.style.display="none"
+  page2.style.display="block"
   page3.style.display="none"
   page4.style.display="none"
   page5.style.display="none"
 })
 
-page3.addEventListener('click', function(){
-  style.display ="adsolute"
-  page1.style.display="none"
+btn3.addEventListener('click', function(){
+  page1.style.display ="none"
   page2.style.display="none"
+  page3.style.display="block"
   page4.style.display="none"
   page5.style.display="none"
 })
 
-page4.addEventListener('click', function(){
-  style.display ="adsolute"
-  page1.style.display="none"
+btn4.addEventListener('click', function(){
+  page1.style.display ="none"
   page2.style.display="none"
   page3.style.display="none"
+  page4.style.display="block"
   page5.style.display="none"
 })
 
-page5.addEventListener('click', function(){
-  style.display ="adsolute"
-  page1.style.display="none"
+btn5.addEventListener('click', function(){
+  page1.style.display ="none"
   page2.style.display="none"
   page3.style.display="none"
   page4.style.display="none"
+  page5.style.display="block"
 })
 
